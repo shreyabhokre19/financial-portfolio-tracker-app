395857f2cd378142548a7ff035aa6159a4ecd9ae
shreyabhokre1 <52531472+shreyabhokre1@users.noreply.github.com>
2020-06-20 20:59:09 +0530

    update
    k

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import Main from './Pages/Main'
 import './App.css';
 
-import Main from './pages/Main/Main';
+
 
 function App() {
   return (
     <div className="App">
-      <Main />
-    </div>
+      <Main/>
+   </div>
   );
 }
 
--- src/components/MyStocks/MyStocks.js
+++ src/components/MyStocks/MyStocks.js
@@ -1,105 +1,81 @@
 import React, { Component } from 'react';
-import './MyStocks.css';
-import Axios from 'axios';
-import { isEqual } from 'lodash';
+import'./Mystocks.css';
+import ApiCall from '../ApiCall';
 
-class MyStocks extends Component {
-    state = {
-        myStocks: {},
-        isError: false
-    }
-
-    shouldComponentUpdate = (nextProps, nextState) => {
-        return isEqual(nextState, this.state)
-    }
-
-    createRows = () => {
-        let rows = {};
-        Object.keys(this.props.myStocks).map(row => {
-            let dataRow = this.props.myStocks[row];
-            rows[dataRow.symbol] = {...dataRow};
-            Axios.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+dataRow.symbol+'&outputsize=compact&apikey=6X6YP8313WMG8Y86')
-                .then(response => {
-                    let serverData = response.data["Time Series (Daily)"];
-                    let today = new Date();
-                    let dayCounter = 0;
-                    if(today.getDay() === 0) { dayCounter = 2; }
-                    if(today.getDay() === 6) { dayCounter = 1; }
-                    let year = today.getFullYear();
-                    let month = parseInt(today.getMonth())+ 1;
-                    let date = parseInt(today.getDate()-dayCounter)<10 ? '0'+parseInt(today.getDate()-dayCounter) : parseInt(today.getDate()-dayCounter);
-                    let finalDate = year+'-'+month+'-'+date;
-                    if(serverData[finalDate.toString()] === undefined){
-                        finalDate = year+'-'+month+'-'+(date-1);
-                    }
-                    console.log(finalDate);
-                    let todayStocks = serverData[finalDate.toString()];
-                    let currentClosingPrice = todayStocks["4. close"];
-                    let calculateProfit = parseInt([currentClosingPrice - dataRow.closingPrice] * dataRow.numberOfShares);
-                    rows[dataRow.symbol].currentClosingPrice = currentClosingPrice;
-                    rows[dataRow.symbol].calculateProfit = calculateProfit;
-                    this.setState({myStocks: rows})
-                })
-                .catch(error => {
-                    console.log(error);
-                    this.setState({
-                        isError: true
-                    })
-                });
-            return true;
-        })
-    }
-
-    render() {
-        if(Object.keys(this.props.myStocks).length > 0 && Object.keys(this.props.myStocks).length !== Object.keys(this.state.myStocks).length && this.state.isError === false) {
-            this.createRows();
+class Mystocks extends Component {
+    constructor(props) {
+        super(props)
+         this.state = {
+            myStocks:this.props.myStocks,
         }
-        let rows = Object.keys(this.state.myStocks).map(stock => {
-            let stockData = this.state.myStocks[stock];
-            let stockRow = (
-                <tr key={stockData.symbol}>
-                    <td>{stockData.symbol}</td>
-                    <td>{stockData.name}</td>
-                    <td>{stockData.numberOfShares}</td>
-                    <td>{stockData.closingPrice}</td>
-                    <td>{stockData.currentClosingPrice}</td>
-                    <td>{stockData.calculateProfit}</td>
-                    <td><button className='StopTrackingBtn' onClick={() => this.props.stopTracking(stockData.symbol)}>Stop Tracking</button></td>
-                </tr>
-            )
-            return stockRow;
-        })
+        
+    }
+    //This will update the props in child whenever state changes in parent.
+    static getDerivedStateFromProps(props, state) {
+        return {
+            myStocks: props.myStocks
+        };
+       }
+    render() {
+            let renderArray=[];
+            let showMessage="";
+            if(this.state.myStocks.length!==0)
+            {
+                renderArray=this.state.myStocks.map((item,index)=>{  
+                    const {Buy_Price,Company_Symbol,Company_name,No_of_Shares}= item;//Destructuring
+                     return(                   
+                        <tr key={index}>   
+                        <td>{Company_Symbol}</td>
+                        <td>{Company_name}</td>
+                        <td>{No_of_Shares}</td>
+                        <td>{Buy_Price}</td>
+                        <ApiCall
+                        symbol={item.Company_Symbol}
+                        buyprice={item.Buy_Price}
+                        share={item.No_of_Shares} />
+                        <td><button className="StopTrackingBtn" onClick={()=>this.props.stopTrackingHandler(index)}>Stop tracking</button></td>
+                        </tr> 
+                        )
+                     }) ;
+            }
+            else{
+                
+                    showMessage=<h1>Loading...You don't own any Stocks!Please Add one by clicking the button below.</h1>;
+                    
+             }
+            
+            
+        
         return (
-            <div className='MyStocks'>
-                <div className='Header'>
-                    <span className='MyStocksTitle'>My Stocks</span>
-                    {/* <input type='text' className='SearchBar' placeholder='Search...' /> */}
-                </div>
-                <div className='Body'>
-                    {
-                        this.state.isError ?
-                        <p>There seems to be a server issue, please try again later.</p> :
-                        <table id='MyStocksTable' className='MyStocksTable'>
-                            <thead>
-                                <tr>
-                                    <th>Stock symbol</th>
-                                    <th>Stock name</th>
-                                    <th>No.of shares</th>
-                                    <th>Buy price</th>
-                                    <th>Current price</th>
-                                    <th>Profit/Loss</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {rows}
-                            </tbody>
+            
+                <div className="MyStocks">
+                <h2>My Stocks</h2>
+                {(this.state.myStocks.length!==0)?
+                         <div className="table_res">
+                         <table className="MyStocksTable">
+                         <thead>
+                         <tr className="heading_row">
+                         <th>Stock symbol</th>
+                         <th>Stock name</th>
+                         <th>No.of share</th>
+                         <th>Buy price</th>
+                         <th>Current price</th>
+                         <th>Profit/Loss</th>
+                         <th>Tracking Status</th>
+                         </tr>
+                         </thead>
+                         <tbody>
+                         {renderArray}
+                         </tbody>
                         </table>
-                    }
-                </div>
-            </div>
+                        </div>
+                        :<div>{showMessage}</div>
+                        
+                }
+        </div>
+        
         )
     }
 }
 
-export default MyStocks;
+export default Mystocks;
